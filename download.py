f577b6acd625208a6c7f60684ac12fd5564b59ec
Alexander Brightwater <alexander.brightwater@gmail.com>
2023-10-09 21:21:53 +0200

    Update download.py; Modify mod files

--- download.py
+++ download.py
@@ -6,90 +6,71 @@ import sys
 # Directory where mod lists are located
 mod_list_directory = "mod-lists"
 
-def download_mod(mod_identifier, modloader_version, minecraft_version, download_folder):
-    # Check if file already exists
-    if os.path.exists(f"./downloads/{download_folder}/{mod_identifier}.jar"):
-        return f"{mod_identifier}.jar already exists. Skipping download."
-
-    # Base URL for Modrinth API
+def download_mod(mod_identifier, modloader_version, minecraft_version, download_folder, mod_prefix):
     base_url = "https://api.modrinth.com/v2"
 
-    # Fetch mod versions
     versions_url = f"{base_url}/project/{mod_identifier}/version?facets=[['versions:{minecraft_version}'],['loader:{modloader_version}']]"
     response = requests.get(versions_url)
+
     if response.status_code != 200:
-        return f"\033[91mError: Unable to fetch mod versions for {mod_identifier}. Status code: {response.status_code}\033[0m"
+        return None
 
     versions = response.json()
-
-    # Filter versions based on modloader and Minecraft version
     filtered_versions = [
         v for v in versions if modloader_version in v['loaders'] and minecraft_version in v['game_versions']
     ]
 
     if not filtered_versions:
-        return f"\033[91mError: No compatible mod version found for {mod_identifier}.\033[0m"
+        return None
 
-    # Download the mod JAR file
     download_url = filtered_versions[0]['files'][0]['url']
     jar_response = requests.get(download_url)
 
     if jar_response.status_code != 200:
-        return f"\033[91mError: Unable to download {mod_identifier}. Status code: {jar_response.status_code}\033[0m"
+        return None
 
-    # Create directory if it doesn't exist
     if not os.path.exists(f'./downloads/{download_folder}'):
         os.makedirs(f'./downloads/{download_folder}')
 
-    # Save the JAR file
-    with open(f"./downloads/{download_folder}/{mod_identifier}.jar", "wb") as f:
+    with open(f"./downloads/{download_folder}/{mod_prefix}{mod_identifier}.jar", "wb") as f:
         f.write(jar_response.content)
 
-    return f"Successfully downloaded {mod_identifier}.jar"
+    return f"Successfully downloaded {modloader_version.upper()} mod: {mod_prefix}{mod_identifier}.jar"
 
-# Command-line argument parsing
 parser = argparse.ArgumentParser(description='Download mods from Modrinth.')
 parser.add_argument('--loader', type=str, default='fabric', help='Mod loader (e.g., fabric, forge)')
 parser.add_argument('--mc_version', type=str, default='1.20.1', help='Minecraft version (e.g., 1.20.1)')
 parser.add_argument('--modlist', type=str, default='mods.list', help='File containing list of mods to download')
+parser.add_argument('--use_fabric', action='store_true', help='Use Fabric as fallback if mod download with initial loader fails')
 args = parser.parse_args()
 
-# Construct the download folder name
 download_folder = f"latest_{args.loader}_{args.mc_version}"
 
-# Read mod identifiers from specified file in the mod-lists directory
 try:
     with open(f"{mod_list_directory}/{args.modlist}", "r") as f:
         mod_identifiers = f.readlines()
 except FileNotFoundError:
-    print(f"\033[91mError: File {mod_list_directory}/{args.modlist} not found.\033[0m")
+    print(f"Error: File {mod_list_directory}/{args.modlist} not found.")
     sys.exit(1)
 
-# Remove any leading or trailing whitespace from each mod identifier
 mod_identifiers = [mod.strip() for mod in mod_identifiers]
-
-# Initialize log for failed downloads
 failed_downloads = []
 
-# Initialize a variable to keep track of the length of the last printed message
-last_message_length = 0
-
-# Download each mod
 for mod_identifier in mod_identifiers:
-    result = download_mod(mod_identifier, args.loader, args.mc_version, download_folder)
+    mod_prefix = 'Q_' if args.loader == 'quilt' else 'F_'
+    result = download_mod(mod_identifier, args.loader, args.mc_version, download_folder, mod_prefix)
 
-    if "Error" in result:
-        print(result)
-        failed_downloads.append(result)
+    if result is None and args.use_fabric:
+        mod_prefix = 'F_'
+        result = download_mod(mod_identifier, 'fabric', args.mc_version, download_folder, mod_prefix)
+
+    if result is None:
+        error_message = f"Error: No compatible mod version found for {mod_identifier}."
+        print(error_message)
+        failed_downloads.append(error_message)
     else:
-        # Clear the last printed message
-        print(" " * last_message_length, end="\r")
+        print(result)
 
-        # Print the new message and update the length of the last printed message
-        print(result, end="\r")
-        last_message_length = len(result)
-        
-# Write failed downloads to log
 if failed_downloads:
     with open(f"./downloads/{download_folder}.log", "w") as f:
         for entry in failed_downloads:
