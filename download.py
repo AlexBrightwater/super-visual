ec793634e55288a965efa8b41d5987e61a3c44a7
Alexander Brightwater <alexander.brightwater@gmail.com>
2023-10-26 00:59:05 +0200

    Fix download.py

--- download.py
+++ download.py
@@ -10,15 +10,15 @@ ENDC = '\033[0m'
 BASE_URL = "https://api.modrinth.com/v2"
 
 def fetch_versions(identifier, minecraft_version, item_type="mod"):
-    if item_type == "mod":
-        versions_url = f"{BASE_URL}/project/{identifier}/version?game_versions=[\"{minecraft_version}\"]"
-    else:
-        versions_url = f"{BASE_URL}/project/{identifier}/version?game_versions=[\"{minecraft_version}\"]"
+    versions_url = f"{BASE_URL}/project/{identifier}/version?game_versions=[\"{minecraft_version}\"]"
     
-    response = requests.get(versions_url)
-    if response.status_code != 200:
+    try:
+        response = requests.get(versions_url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"{RED}Error fetching versions: {e}{ENDC}")
         return None
-    return response.json()
 
 def filter_versions(versions, modloader_version, minecraft_version, item_type="mod"):
     if item_type == "mod":
@@ -26,7 +26,7 @@ def filter_versions(versions, modloader_version, minecraft_version, item_type="m
     else:
         return [v for v in versions if minecraft_version in v['game_versions']]
 
-def download_file(identifier, modloader_version, minecraft_version, download_folder, prefix, item_type="mod"):
+def download_file(identifier, modloader_version, minecraft_version, download_folder, suffix, item_type="mod"):
     versions = fetch_versions(identifier, minecraft_version, item_type)
     if versions is None:
         return None
@@ -36,23 +36,24 @@ def download_file(identifier, modloader_version, minecraft_version, download_fol
         return None
 
     download_url = filtered_versions[0]['files'][0]['url']
-    response = requests.get(download_url)
-    if response.status_code != 200:
+    try:
+        response = requests.get(download_url)
+        response.raise_for_status()
+
+        download_path = f'./downloads/{download_folder}'
+        os.makedirs(download_path, exist_ok=True)
+
+        file_extension = ".jar" if item_type == "mod" else ".zip"
+        with open(f"{download_path}/{identifier}_{suffix}{file_extension}", "wb") as f:
+            f.write(response.content)
+        return f"Successfully downloaded {item_type}: {identifier}_{suffix}{file_extension}"
+    except requests.RequestException as e:
+        print(f"{RED}Error downloading file: {e}{ENDC}")
         return None
-
-    download_path = f'./downloads/{download_folder}'
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
-
-
-    file_extension = ".jar" if item_type == "mod" else ".zip"
-    with open(f"{download_path}/{prefix}{identifier}{file_extension}", "wb") as f:
-        f.write(response.content)
-    return f"Successfully downloaded {item_type}: {prefix}{identifier}{file_extension}"
 
 def main():
     parser = argparse.ArgumentParser(description='Download mods and resource packs from Modrinth.')
-    parser.add_argument('--loader', type=str, default='fabric', help='Mod loader (e.g., fabric, forge)')
+    parser.add_argument('--loader', type=str, default='fabric', choices=['fabric', 'quilt', 'neo-forge', 'forge'], help='Mod loader (e.g., fabric, forge)')
     parser.add_argument('--mc_version', type=str, default='1.20.1', help='Minecraft version (e.g., 1.20.1)')
     parser.add_argument('--mod', type=str, help='Single mod to download')
     parser.add_argument('--modlist', type=str, help='File containing list of mods to download')
@@ -95,15 +96,19 @@ def main():
     failed_downloads = []
 
     for identifier in identifiers:
-        if item_type == "mod":
-            prefix = 'Q_' if args.loader == 'quilt' else 'F_'
+        if args.loader == 'quilt':
+            suffix = 'quilt'
+        elif args.loader == 'neo-forge':
+            suffix = 'neo-forge'
+        elif args.loader == 'forge':
+            suffix = 'forge'
         else: 
-            prefix = ""
+            suffix = "fabric"
             
-        result = download_file(identifier, args.loader, args.mc_version, download_folder, prefix, item_type)
+        result = download_file(identifier, args.loader, args.mc_version, download_folder, suffix, item_type)
         if result is None and args.use_fabric and item_type == "mod":
-            prefix = 'F_'
-            result = download_file(identifier, 'fabric', args.mc_version, download_folder, prefix, item_type)
+            suffix = 'fabric'  # Changed this line to fix the issue
+            result = download_file(identifier, 'fabric', args.mc_version, download_folder, suffix, item_type)
         if result is None:
             error_message = f"Error: No compatible {item_type} version found for {identifier}."
             print(f"{RED}{error_message}{ENDC}")
