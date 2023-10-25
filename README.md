caff9e52718a07b3d79594ec1e1f741f9eb3103e
Alexander Brightwater <alexander.brightwater@gmail.com>
2023-10-26 00:36:50 +0200

    Finish Reource Pack Support

--- README.md
+++ README.md
@@ -2,20 +2,53 @@
 This is the GitHub Repo for Super Visual, a Modrinth Modpack. This repo focuses on the automatisation part of the modpack creation, it also stores some static files that need a place to live :)
 
 # downlaod.py
-This script downloads the latest versions of minecraft mods based off a few parameters.  
+A simple script to download Minecraft mods and resource packs from Modrinth.
 
-`--modlist:` Sets the file with your modlist (default: mods.list)  
-`--loader:` Sets the loader for the mods (default: fabric)  
-`--mc_version:` Sets the Minecraft Version for the mods you want to download (default: 1.20.1)  
-`--use_fabric:` Use Fabric as a fallback if the mod isn't found for the specified loader. Only works when --loader is set to 'quilt'.  
+## Features
 
-## Exapmple Command
-```bash
-python3 download.py --loader fabric --mc_version 1.20.1 --modlist mods.list
-python3 download.py --loader quilt --mc_version 1.18.2 --use_fabric
+- Download individual mods or resource packs by project_id or slug.
+- Batch download mods or resource packs using a list.
+- Best used with Fabric or Quilt.
+- Error logging for failed downloads.
 
-```
+## Usage
 
-## Mod List file
-The file is expected to be in a dir called mod-lists, but that can be changed through the var mod_list_directory at top of the script.
-Just store one mod per line in this file without blank lines. Take the name from the URL of the mod's modrinth page or use it's project_id.
+### Required Arguments:
+
+- `--mc_version`: The Minecraft version for which you want to download the mod or resource pack. (Default is `1.20.1`)
+
+### Optional Arguments:
+
+- `--loader`: Choose the mod loader (either `fabric` or `forge`). Default is `fabric`.
+- `--mod`: Specify the identifier of a single mod to download.
+- `--modlist`: Specify a file containing a list of mod identifiers to download.
+- `--tex`: Specify the identifier of a single resource pack to download.
+- `--texlist`: Specify a file containing a list of resource pack identifiers to download.
+- `--use_fabric`: Use this flag to fallback to Fabric if the mod download with the initial loader fails.
+- `--name`: Custom name for the download directory.
+
+### Example Commands:
+
+1. Download a single mod for Fabric loader:
+   ```
+   python3 download.py --mod mod_identifier
+   ```
+
+2. Download mods from a list for Quilt loader and falling back to fabric if no quilt version is available:
+   ```
+   python3 download.py --loader quilt --modlist lists/mod_list_file.txt --use_fabric
+   ```
+
+3. Download resource packs from a list and use a custom download folder name:
+   ```
+   python3 download.py --texlist lists/texture_pack_list_file.list --name texture_collection
+   ```
+
+## Notes:
+
+- Mods are saved with a prefix of `F_` for Fabric, `Q_` for Quilt.
+- Resource packs do not have a prefix.
+- The default download directory is `./downloads/latest_{loader}_{mc_version}`.
+- A log file is created in the download directory for any failed downloads.
+- Lists are one id/slug per line
+
--- download.py
+++ download.py
@@ -7,125 +7,114 @@ import sys
 RED = '\033[91m'
 ENDC = '\033[0m'
 
-# Directory where mod lists are located
-mod_list_directory = "mod-lists"
+BASE_URL = "https://api.modrinth.com/v2"
 
-def download_mod(mod_identifier, modloader_version, minecraft_version, download_folder, mod_prefix):
-    base_url = "https://api.modrinth.com/v2"
-
-    #https://api.modrinth.com/v2/project/sodium/version?loaders=["fabric"]&game_versions=["1.20.1"]
-    if modloader_version not in globals():
-        versions_url = f"{base_url}/project/{mod_identifier}/version?game_versions=[\"{minecraft_version}\"]"
-        print("RESOURCEPACK")
-        response = requests.get(versions_url)
-        print(versions_url)
-
-        if response.status_code != 200:
-            return None
-
-        versions = response.json()
-        filtered_versions = [
-            v for v in versions if minecraft_version in v['game_versions']
-        ]
+def fetch_versions(identifier, minecraft_version, item_type="mod"):
+    if item_type == "mod":
+        versions_url = f"{BASE_URL}/project/{identifier}/version?game_versions=[\"{minecraft_version}\"]"
     else:
-        versions_url = f"{base_url}/project/{mod_identifier}/version?loaders=[\"{modloader_version}\"]&game_versions=[\"{minecraft_version}\"]"
-        response = requests.get(versions_url)
-        print(versions_url)
+        versions_url = f"{BASE_URL}/project/{identifier}/version?game_versions=[\"{minecraft_version}\"]"
+    
+    response = requests.get(versions_url)
+    if response.status_code != 200:
+        return None
+    return response.json()
 
-        if response.status_code != 200:
-            return None
+def filter_versions(versions, modloader_version, minecraft_version, item_type="mod"):
+    if item_type == "mod":
+        return [v for v in versions if modloader_version in v['loaders'] and minecraft_version in v['game_versions']]
+    else:
+        return [v for v in versions if minecraft_version in v['game_versions']]
 
-        versions = response.json()
-        filtered_versions = [
-            v for v in versions if modloader_version in v['loaders'] and minecraft_version in v['game_versions']
-        ]
+def download_file(identifier, modloader_version, minecraft_version, download_folder, prefix, item_type="mod"):
+    versions = fetch_versions(identifier, minecraft_version, item_type)
+    if versions is None:
+        return None
 
-
+    filtered_versions = filter_versions(versions, modloader_version, minecraft_version, item_type)
     if not filtered_versions:
         return None
 
     download_url = filtered_versions[0]['files'][0]['url']
-    jar_response = requests.get(download_url)
-
-    if jar_response.status_code != 200:
+    response = requests.get(download_url)
+    if response.status_code != 200:
         return None
 
-    if not os.path.exists(f'./downloads/{download_folder}'):
-        os.makedirs(f'./downloads/{download_folder}')
+    download_path = f'./downloads/{download_folder}'
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
 
-    if modloader_version not in globals():
-        with open(f"./downloads/{download_folder}/{mod_prefix}{mod_identifier}.zip", "wb") as f:
-            f.write(jar_response.content)
-        if modloader_version == "quilt":
-            return f"Successfully downloaded {modloader_version.upper()}" + "  mod: " + f"{mod_prefix}{mod_identifier}.zip"
+
+    file_extension = ".jar" if item_type == "mod" else ".zip"
+    with open(f"{download_path}/{prefix}{identifier}{file_extension}", "wb") as f:
+        f.write(response.content)
+    return f"Successfully downloaded {item_type}: {prefix}{identifier}{file_extension}"
+
+def main():
+    parser = argparse.ArgumentParser(description='Download mods and resource packs from Modrinth.')
+    parser.add_argument('--loader', type=str, default='fabric', help='Mod loader (e.g., fabric, forge)')
+    parser.add_argument('--mc_version', type=str, default='1.20.1', help='Minecraft version (e.g., 1.20.1)')
+    parser.add_argument('--mod', type=str, help='Single mod to download')
+    parser.add_argument('--modlist', type=str, help='File containing list of mods to download')
+    parser.add_argument('--tex', type=str, help='Single Resourcepack to download')
+    parser.add_argument('--texlist', type=str, help='File containing list of Resource Packs to download')
+    parser.add_argument('--use_fabric', action='store_true', help='Use Fabric as fallback if mod download with initial loader fails')
+    parser.add_argument('--name', type=str, help='Custom download directory name')
+    args = parser.parse_args()
+
+    download_folder = args.name or f"latest_{args.loader}_{args.mc_version}"
+
+    identifiers = []
+    item_type = ""
+    if args.mod:
+        identifiers.append(args.mod)
+        item_type = "mod"
+    elif args.modlist:
+        try:
+            with open(f"{args.modlist}", "r") as f:
+                identifiers = f.readlines()
+            item_type = "mod"
+        except FileNotFoundError:
+            error_message = f"Error: File {args.modlist} not found."
+            print(f"{RED}{error_message}{ENDC}")
+            sys.exit(1)
+    elif args.tex:
+        identifiers.append(args.tex)
+        item_type = "tex"
+    elif args.texlist:
+        try:
+            with open(f"{args.texlist}", "r") as f:
+                identifiers = f.readlines()
+            item_type = "tex"
+        except FileNotFoundError:
+            error_message = f"Error: File {args.texlist} not found."
+            print(f"{RED}{error_message}{ENDC}")
+            sys.exit(1)
+
+    identifiers = [identifier.strip() for identifier in identifiers]
+    failed_downloads = []
+
+    for identifier in identifiers:
+        if item_type == "mod":
+            prefix = 'Q_' if args.loader == 'quilt' else 'F_'
+        else: 
+            prefix = ""
+            
+        result = download_file(identifier, args.loader, args.mc_version, download_folder, prefix, item_type)
+        if result is None and args.use_fabric and item_type == "mod":
+            prefix = 'F_'
+            result = download_file(identifier, 'fabric', args.mc_version, download_folder, prefix, item_type)
+        if result is None:
+            error_message = f"Error: No compatible {item_type} version found for {identifier}."
+            print(f"{RED}{error_message}{ENDC}")
+            failed_downloads.append(error_message)
         else:
-            return f"Successfully downloaded {modloader_version.upper()} mod: {mod_prefix}{mod_identifier}.zip"
-    else:
-        with open(f"./downloads/{download_folder}/{mod_prefix}{mod_identifier}.jar", "wb") as f:
-            f.write(jar_response.content)
-        if modloader_version == "quilt":
-            return f"Successfully downloaded {modloader_version.upper()}" + "  mod: " + f"{mod_prefix}{mod_identifier}.jar"
-        else:
-            return f"Successfully downloaded {modloader_version.upper()} mod: {mod_prefix}{mod_identifier}.jar"
+            print(result)
 
-    
+    if failed_downloads:
+        with open(f"./downloads/{download_folder}.log", "w") as f:
+            for entry in failed_downloads:
+                f.write(f"{entry}\n")
 
-parser = argparse.ArgumentParser(description='Download mods from Modrinth.')
-parser.add_argument('--loader', type=str, default='fabric', help='Mod loader (e.g., fabric, forge)')
-parser.add_argument('--mc_version', type=str, default='1.20.1', help='Minecraft version (e.g., 1.20.1)')
-parser.add_argument('--mod', type=str, help='Single mod to download')
-parser.add_argument('--modlist', type=str, help='File containing list of mods to download')
-parser.add_argument('--use_fabric', action='store_true', help='Use Fabric as fallback if mod download with initial loader fails')
-parser.add_argument('--name', type=str, help='Custom download directory name')
-args = parser.parse_args()
-
-if args.name:
-    download_folder = args.name
-else:
-    download_folder = f"latest_{args.loader}_{args.mc_version}"
-
-mod_identifiers = []
-
-if args.mod:
-    mod_identifiers.append(args.mod)
-elif args.modlist:
-    try:
-        with open(f"{mod_list_directory}/{args.modlist}", "r") as f:
-            mod_identifiers = f.readlines()
-    except FileNotFoundError:
-        error_message = f"Error: File {mod_list_directory}/{args.modlist} not found."
-        print(f"{RED}{error_message}{ENDC}")
-        sys.exit(1)
-
-mod_identifiers = [mod.strip() for mod in mod_identifiers]
-failed_downloads = []
-
-# Check if basemods list file exists for the loader when using --modlist
-if args.modlist:
-    basemods_file = f"{mod_list_directory}/{args.loader}-basemods.list"
-    if os.path.exists(basemods_file):
-        with open(basemods_file, "r") as f:
-            basemods_identifiers = f.readlines()
-        basemods_identifiers = [mod.strip() for mod in basemods_identifiers]
-        mod_identifiers += basemods_identifiers  # Add basemods to the list
-
-for mod_identifier in mod_identifiers:
-    mod_prefix = 'Q_' if args.loader == 'quilt' else 'F_'
-    result = download_mod(mod_identifier, args.loader, args.mc_version, download_folder, mod_prefix)
-
-    if result is None and args.use_fabric:
-        mod_prefix = 'F_'
-        result = download_mod(mod_identifier, 'fabric', args.mc_version, download_folder, mod_prefix)
-
-    if result is None:
-        error_message = f"Error: No compatible mod version found for {mod_identifier}."
-        print(f"{RED}{error_message}{ENDC}")
-        failed_downloads.append(error_message)
-    else:
-        print(result)
-
-if failed_downloads:
-    with open(f"./downloads/{download_folder}.log", "w") as f:
-        for entry in failed_downloads:
-            f.write(f"{entry}\n")
-
+if __name__ == "__main__":
+    main()
